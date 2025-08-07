599ff8d520e889044bd9cc6c12d6cfd1fdeb2d34
khgty1226 <khgty1226@naver.com>
2025-08-07 11:41:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import styles from "./App.module.css"
+import {useState, useEffect} from "react";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+function App() {
+    return <Router>
+        <Routes>
+            <Route path="/movie/:id" element={<Detail/>} />
+            <Route path="/" element={<Home/>} />
+        </Routes>
+    </Router>;
+}
+export default App;
+// for commit
